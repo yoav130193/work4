0775a6cafdedcd859b1c0607b6c14b7a26cf611f
raz <raz@icohens.com>
2019-01-09 21:22:55 +0200

    njkbk

--- schedule .py
+++ schedule .py
@@ -1,6 +1,23 @@
 import os
-import sys
 import sqlite3
+
+def print_table():
+    print('courses')
+    cursor.execute("SELECT * FROM courses")
+    list = cursor.fetchall()
+
+    for element in list:
+        print(str(element))
+    print('classrooms')
+    cursor.execute("SELECT * FROM classrooms")
+    list = cursor.fetchall()
+    for element in list:
+        print(str(element))
+    print('students')
+    cursor.execute("SELECT * FROM students")
+    list = cursor.fetchall()
+    for element in list:
+        print(str(element))
 
 
 def courses_not_done():
@@ -9,83 +26,82 @@ def courses_not_done():
     return num_of_courses[0] > 0
 
 
-# TODO-
+def assign_course(class_id, location):
+    cursor.execute('SELECT id, course_name, course_length, student, number_of_students '
+                   'FROM courses '
+                   'WHERE class_id = ?', (class_id,))
+    course = cursor.fetchone()
+    if course is not None:
+        print("(" + str(iteration_number) + ") " + location + ": " + course[1] + " is schedule to start")
+        cursor.execute('UPDATE classrooms '
+                       'SET current_course_id = ?, '
+                       'current_course_time_left = ? '
+                       'WHERE id = ?', (course[0], course[2], class_id))
+        cursor.execute('UPDATE students '
+                       'SET count = count - ? '
+                       'WHERE grade = ?', (course[4], course[3]))
+
+
+def decreases_count(course_id, location, num):
+    cursor.execute('SELECT course_name '
+                   'FROM courses '
+                   'WHERE id = ?', (course_id,))
+    course_name = cursor.fetchone()
+    if course_name is not None:
+        if num > 1:
+            print("(" + str(iteration_number) + ") " + location + ": occupied by " + course_name[0])
+        cursor.execute('UPDATE classrooms '
+                       'SET current_course_time_left = current_course_time_left - 1 '
+                       'WHERE current_course_id = ?', (course_id,))
+    cursor.execute('SELECT current_course_time_left '
+                   'FROM classrooms '
+                   'WHERE current_course_id = ?', (course_id,))
+    left = cursor.fetchone()
+    if left[0] != 0:
+        cursor.execute('UPDATE classrooms '
+                       'SET current_course_time_left = 0'
+                       'WHERE current_course_id = ?', (course_id,))
+
+
+
+def remove_course(course_id, location):
+    cursor.execute('SELECT course_name '
+                   'FROM courses '
+                   'WHERE id = ?', (course_id,))
+    course_name = cursor.fetchone()
+    if course_name is not None:
+        print("(" + str(iteration_number) + ") " + location + ": " + course_name[0] + " is done")
+        cursor.execute('DELETE '
+                       'FROM courses '
+                       'WHERE id = ?', (course_id,))
+
 
 databaseexisted = os.path.isfile('schedule.db')
 
 dbcon = sqlite3.connect('schedule.db')
+
 with dbcon:
     cursor = dbcon.cursor()
 
     iteration_number = 0
     while databaseexisted and courses_not_done():
-
-        # decrease all courses time
-        cursor.execute('UPDATE classrooms '
-                       'SET current_course_time_left = current_course_time_left - 1 '
-                       'WHERE current_course_time_left >0')
-        dbcon.commit();
-        # print and delete all courses that are finished
-        cursor.execute(("SELECT co.course_name,cl.location "
-                        "FROM "
-                        "classrooms as cl, "
-                        "courses as co "
-                        "WHERE cl.current_course_time_left=0 AND cl.current_course_id = co.id"))
-
-        done_courses = cursor.fetchall()
-        for i in range(0, len(done_courses)):
-            done_courses[i] = "(" + str(iteration_number) + ") " + done_courses[i][1] + ": " + done_courses[i][
-                0] + " is done"
-        cursor.execute("SELECT current_course_id "
-                       "FROM classrooms "
-                       "WHERE current_course_time_left=0 "
-                       "AND current_course_id!=0")
-        delete_this = cursor.fetchall();
-        for i in range(0, len(delete_this)):
-            id = delete_this[i]
-            cursor.execute("DELETE "
-                           "FROM courses "
-                           "WHERE id=?", (id))
-        # update classroom that now available
-        cursor.execute("UPDATE classrooms "
-                       "SET current_course_id = 0 "
-                       "WHERE current_course_time_left=0")
-        dbcon.commit();
-        # print courses that are occupied
-        cursor.execute(("SELECT co.course_name,cl.location "
-                        "FROM classrooms as cl "
-                        "JOIN courses as co "
-                        "ON cl.current_course_time_left>0 AND cl.current_course_id = co.id"))
-        occupied_classrooms = cursor.fetchall()
-        for i in range(0, len(occupied_classrooms)):
-            occupied_classrooms[i] = "(" + str(iteration_number) + ") " + occupied_classrooms[i][1] + ": occupied by " + \
-                                     occupied_classrooms[i][0]
-
-        # add courses to classrooms
-        cursor.execute("SELECT id,location FROM classrooms WHERE current_course_time_left=0")
-        list_of_free_classroom = cursor.fetchall()
-        i = 0
-        while (i < len(list_of_free_classroom)):
-            id = list_of_free_classroom[i][0]
-            cursor.execute("SELECT id, course_length,course_name FROM courses WHERE class_id=? ", (id,))
-            one_course = cursor.fetchone()
-            if one_course is not None:
-                list_of_free_classroom[i] = "(" + str(iteration_number) + ") " + list_of_free_classroom[i][1] + ": " + \
-                                            one_course[2] + " is schedule to start"
-                cursor.execute("UPDATE classrooms  "
-                               "SET current_course_id = ?, "
-                               "current_course_time_left = ? "
-                               "WHERE id= ?", (one_course[0], one_course[1], id,))
-                i = i + 1
-            else:
-                del (list_of_free_classroom[i])
-
-        for element in list_of_free_classroom:
-            print(element)
-        for element in occupied_classrooms:
-            print(element)
-        for element in done_courses:
-            print(element)
-
-        dbcon.commit()
+        cursor.execute('SELECT * '
+                       'FROM classrooms')
+        classrooms = cursor.fetchall()
+        for one_classroom in classrooms:
+            if one_classroom[3] == 0:
+                assign_course(one_classroom[0], one_classroom[1])
+            elif one_classroom[3] > 0:
+                decreases_count(one_classroom[2], one_classroom[1], one_classroom[3])
+            cursor.execute('SELECT * '
+                           'FROM classrooms '
+                           'WHERE id = ?', (one_classroom[0],))
+            one_classroom = cursor.fetchone();
+            if one_classroom[3] == 0:
+                remove_course(one_classroom[2], one_classroom[1])
+                assign_course(one_classroom[0], one_classroom[1])
+        print_table()
         iteration_number = iteration_number + 1
+
+dbcon.commit()
+
