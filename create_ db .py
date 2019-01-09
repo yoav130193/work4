67f8117fcc784a56ca73914dde87c29e2b427468
yoav <yoav130193@gmail.com>
2019-01-09 14:33:11 +0200

    j

--- create_ db .py
+++ create_ db .py
@@ -1,7 +1,14 @@
 import sqlite3
 import os
 
+databaseexisted = os.path.isfile('schedule.db')
+
 dbcon = sqlite3.connect('schedule.db')
 with dbcon:
     cursor = dbcon.cursor()
-    cursor.execute("CREATE TABLE courses(id INTEGER PRIMARY KEY, NAME TEXT NOT NULL)")  # create table students
+    cursor.execute("CREATE TABLE courses(id INTEGER PRIMARY KEY, course_name TEXT NOT NULL, "
+                   "student TEXT NOT NULL, number_of_students INTEGER NOT NULL,"
+                   " class_id INTEGER REFERENCES classrooms(id), number_of_students INTEGER NOT NULL)")  # create table courses
+    cursor.execute("CREATE TABLE students(grade TEXT PRIMARY KEY, count INTEGER NOT NULL)")  # create table students
+    cursor.execute("CREATE TABLE classrooms(id INTEGER PRIMARY KEY, location TEXT NOT NULL, "
+                   "current_course_id INTEGER NOT NULL, current_course_time_left INTEGER NOT NULL)")  # create table classrooms
